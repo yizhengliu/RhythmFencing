0ad32348dd0be84e9e16ccb2a9df3368c7dbb19d
JiheiBot <2757174746@qq.com>
2022-11-15 13:30:33 +0000

    Modification
    I added manual timing rather than the constant value. And added multiple actions for the character.

--- RhythmFencing/Assets/Scripts/AdvancedSpawner.cs
+++ RhythmFencing/Assets/Scripts/AdvancedSpawner.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class AdvancedSpawner : MonoBehaviour
 {
+    private AudioSource m_MyAudioSource;
     public GameObject Enemy;
     public Transform[] SpawnPoints;
     public float beat;
-    private float timer;
+    public int enemiesToSpawn = 0;
+    private float[] timing;
+    private int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_MyAudioSource = GetComponent<AudioSource>();
+        TextAsset textAsset = Resources.Load("Labels 1") as TextAsset;
 
+        string[] textInfo = textAsset.text.Split('\n');
+        timing = new float[textInfo.Length];
+
+        for (int i = 0; i < textInfo.Length; i++)
+            timing[i] = float.Parse(textInfo[i]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > beat)
+        if (m_MyAudioSource.time > timing[counter] && counter < timing.Length)
         {
+            counter++;
             GameObject newEnemy = Instantiate(Enemy, SpawnPoints[Random.Range(0, 4)]);
-            timer -= beat;
         }
-
-        timer += Time.deltaTime;
     }
 }
--- RhythmFencing/Assets/Scripts/AnimationStateController.cs
+++ RhythmFencing/Assets/Scripts/AnimationStateController.cs
@@ -19,11 +19,26 @@ public class AnimationStateController : MonoBehaviour
         //if the z coordi is reached
         if (animationInfo.IsName("Sword And Shield Run"))
         {
-            if (Vector3.Distance(transform.position, Vector3.zero) < 3) 
-                animator.SetBool("StartAttack", true);
+            
+            if (Vector3.Distance(transform.position, Vector3.zero) < 3)
+
+                switch (Random.Range(0, 3))
+                {
+                    case 0:
+                        animator.SetBool("StartAttack", true);
+                        break;
+                    case 1:
+                        animator.SetBool("RotateSlash", true);
+                        break;
+                    case 2:
+                        animator.SetBool("NormalSlash", true);
+                        break;
+                }
                 
         }
-        else if (animationInfo.IsName("Sword And Shield Slash")) {
+        else if (animationInfo.IsName("Sword And Shield Slash") 
+            || animationInfo.IsName("Sword And Shield Attack") 
+            || animationInfo.IsName("Sword And Shield Normal Slash")) {
             if (animationInfo.normalizedTime > 1.0f) {
                 Destroy(this.gameObject);
             }
